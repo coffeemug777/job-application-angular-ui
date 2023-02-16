b2da51ef85cb0a1a03b9b4af4dac78a6ced98ada
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-16 16:15:46 -0500

    add guard to detect if user already logged in, placeholder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserLoginComponent } from './components/user-login/user-login.component';
+import { IsLoggedInGuard } from './guards/is-logged-in.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: UserLoginComponent,
+    canActivate: [IsLoggedInGuard],
   },
 ];
 
