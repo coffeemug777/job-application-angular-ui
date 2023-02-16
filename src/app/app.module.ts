30cd53a17a9b055ca947a8e7ae98f9dd7ee1d3f1
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-16 15:49:26 -0500

    add placeholder components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { OpeningsComponent } from './components/openings/openings.component';
+import { StatusComponent } from './components/status/status.component';
+import { ApplyComponent } from './components/apply/apply.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    UserRegisterComponent,
+    DashboardComponent,
+    OpeningsComponent,
+    StatusComponent,
+    ApplyComponent
   ],
   imports: [
     BrowserModule,
