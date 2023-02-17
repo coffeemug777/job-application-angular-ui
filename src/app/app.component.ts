7c896cc4f45ddeeeea116c1645da6626dbf9c918
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-16 21:11:43 -0500

    Add store read on app load, add logic to redirect if user logged in already

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { firstValueFrom } from 'rxjs';
+import { User } from './stores/user.reducer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-ui';
+  constructor(private store: Store<User>) {}
+
+  async ngOnInit() {
+    const userFromStore = await firstValueFrom(this.store);
+    if (userFromStore.userLoggedIn) {
+      console.log('redirect to dash');
+      //redirect to dashboard
+    }
+  }
 }
