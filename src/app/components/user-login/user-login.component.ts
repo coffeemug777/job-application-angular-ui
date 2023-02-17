9aef8d26b3cd6679c1c385601f79130c4e81167c
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-16 21:33:54 -0500

    Add router, and if user is defined then route to dashboard

--- src/app/components/user-login/user-login.component.ts
+++ src/app/components/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -10,13 +11,17 @@ export class UserLoginComponent {
   password = '';
   email = '';
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   loginClick() {
     console.log('loginclick');
     if (this.password !== '' && this.email !== '') {
-      console.log('');
-      const login = this.userService.login(this.email, this.password);
+      const user = this.userService.login(this.email, this.password);
+      console.log('login is ', user);
+      if (user) {
+        //redirect to dashboard;
+        this.router.navigate(['dashboard'], { state: { user: user } });
+      }
     }
   }
 
