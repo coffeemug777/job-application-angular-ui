1dca33075168a96a935224676d399664cf485c5f
Denny Wibowo <indocoffee@gmail.com>
2023-02-17 10:21:54 -0500

    Add apply component with opening from link, and display the title, back button and description

--- src/app/components/apply/apply.component.ts
+++ src/app/components/apply/apply.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
+import { Opening, OpeningService } from 'src/app/services/opening.service';
 
 @Component({
   selector: 'app-apply',
   templateUrl: './apply.component.html',
-  styleUrls: ['./apply.component.scss']
+  styleUrls: ['./apply.component.scss'],
 })
 export class ApplyComponent {
+  openingId: string = '';
+  opening: Opening | null = null;
 
+  constructor(
+    private router: ActivatedRoute,
+    private openingService: OpeningService
+  ) {}
+
+  async ngOnInit() {
+    const params = await firstValueFrom(this.router.params);
+    this.openingId = params['id'] ? params['id'] : '';
+
+    if (this.openingId !== '') {
+      this.opening = this.openingService.get(this.openingId);
+    }
+  }
 }
