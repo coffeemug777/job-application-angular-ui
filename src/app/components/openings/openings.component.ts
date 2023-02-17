3b0d648341d5f285d17324d87497a6d118083706
Denny Wibowo <indocoffee@gmail.com>
2023-02-17 09:34:31 -0500

    display openings

--- src/app/components/openings/openings.component.ts
+++ src/app/components/openings/openings.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Opening, OpeningService } from 'src/app/services/opening.service';
 
 @Component({
   selector: 'app-openings',
   templateUrl: './openings.component.html',
-  styleUrls: ['./openings.component.scss']
+  styleUrls: ['./openings.component.scss'],
 })
 export class OpeningsComponent {
+  openings: Opening[] = [];
+  constructor(private openingService: OpeningService) {}
 
+  async ngOnInit() {
+    this.openings = await this.openingService.getAll();
+  }
 }
