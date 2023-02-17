7be953f3e4a99e8547fe37a79325779db98de90b
Denny Wibowo <indocoffee@gmail.com>
2023-02-17 16:13:55 -0500

    Add reactive forms grouping and setup

--- src/app/components/apply/apply.component.ts
+++ src/app/components/apply/apply.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Form, FormArray } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
 import { Opening, OpeningService } from 'src/app/services/opening.service';
 
@@ -12,10 +13,44 @@ export class ApplyComponent {
   openingId: string = '';
   opening: Opening | null = null;
 
+  applicationForm: FormGroup;
+
   constructor(
     private router: ActivatedRoute,
-    private openingService: OpeningService
-  ) {}
+    private openingService: OpeningService,
+    private fb: FormBuilder
+  ) {
+    this.applicationForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      exps: this.fb.array([]),
+    });
+  }
+
+  newExperience() {
+    return this.fb.group({
+      company: '',
+      title: '',
+      startDate: '',
+      endDate: '',
+      description: '',
+      isCurrent: false,
+    });
+  }
+
+  addExperience() {
+    return this.experiences.push(this.newExperience());
+  }
+
+  removeExperience(expIndex: number) {
+    this.experiences.removeAt(expIndex);
+  }
+
+  submitClick() {
+    console.log(this.experiences);
+  }
 
   async ngOnInit() {
     const params = await firstValueFrom(this.router.params);
@@ -24,5 +59,9 @@ export class ApplyComponent {
     if (this.openingId !== '') {
       this.opening = this.openingService.get(this.openingId);
     }
+  }
+
+  get experiences(): FormArray {
+    return this.applicationForm.get('exps') as FormArray;
   }
 }
