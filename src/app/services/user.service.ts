4df8d6391d71d1f0537ccf951d6dcab6066bd8d2
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-16 21:33:26 -0500

    Add lookup maps for user hardcoded for now, and return user based on it

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@angular/core';
+import { UserInfo } from '../stores/user.reducer';
+
+const users: UserInfo[] = [
+  {
+    id: '1',
+    email: 'indocoffee@gmail.com',
+    password: 'test123',
+  },
+  {
+    id: '2',
+    email: 'test@test.com',
+    password: 'test123',
+  },
+];
+
+const userLookup = new Map();
+users.forEach((user) => {
+  userLookup.set(user.email, user);
+});
 
 @Injectable({
   providedIn: 'root',
@@ -8,5 +27,12 @@ export class UserService {
 
   login(email: string, password: string) {
     console.log('service ', email, password);
+    if (
+      userLookup.has(email) &&
+      userLookup.get(email)['password'] === password
+    ) {
+      return userLookup.get(email);
+    }
+    return null;
   }
 }
