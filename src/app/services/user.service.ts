b8fa0cdb0c8260bbd9fa50463570ff07892be1e6
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-22 13:55:26 -0500

    Remove local references

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,24 +3,6 @@ import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { UserInfo } from '../stores/user.reducer';
 
-const users: UserInfo[] = [
-  {
-    id: '1',
-    email: 'indocoffee@gmail.com',
-    password: 'test123',
-  },
-  {
-    id: '2',
-    email: 'test@test.com',
-    password: 'test123',
-  },
-];
-
-const userLookup = new Map();
-users.forEach((user) => {
-  userLookup.set(user.email, user);
-});
-
 const userApiUrl = 'http://localhost:8080/api/user/';
 
 @Injectable({
@@ -39,24 +21,13 @@ export class UserService {
   }
 
   login(email: string, password: string) {
-    if (
-      userLookup.has(email) &&
-      userLookup.get(email)['password'] === password
-    ) {
-      return userLookup.get(email);
     }
-    return null;
   }
 
   async register(email: string, password: string): Promise<boolean | null> {
-    if (userLookup.has(email)) {
-      return null;
-      // error email already exist
-    } else {
-      const response = await firstValueFrom(
-        this.http.post(userApiUrl + 'register', { email, password })
-      );
-      return response as boolean;
-    }
+    const response = await firstValueFrom(
+      this.http.post(userApiUrl + 'register', { email, password })
+    );
+    return response as boolean;
   }
 }
