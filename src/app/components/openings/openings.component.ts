1f53eb83c47626ab1bfafb7b7287af4aad97660c
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-22 21:30:04 -0500

    Add conditions to show 'apply', 'continute' and 'checkStatus' for applications based on conditions

--- src/app/components/openings/openings.component.ts
+++ src/app/components/openings/openings.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Opening, OpeningService } from 'src/app/services/opening.service';
+import { UserService } from 'src/app/services/user.service';
+import { UserInfo } from 'src/app/stores/user.reducer';
 
 @Component({
   selector: 'app-openings',
@@ -8,9 +10,42 @@ import { Opening, OpeningService } from 'src/app/services/opening.service';
 })
 export class OpeningsComponent {
   openings: Opening[] = [];
-  constructor(private openingService: OpeningService) {}
+  currentUser: UserInfo | null = null;
+  constructor(
+    private openingService: OpeningService,
+    private userService: UserService
+  ) {}
+
+  showContinue(openingId: string) {
+    return (
+      this.openings
+        .find((opening) => opening.id === openingId)
+        ?.incompleteApplications.find(
+          (application) => application.userId === this.currentUser?.email
+        ) !== undefined
+    );
+  }
+
+  showCheckStatus(openingId: string) {
+    return (
+      this.openings
+        .find((opening) => opening.id === openingId)
+        ?.completedApplications.find(
+          (application) => application.userId === this.currentUser?.email
+        ) !== undefined
+    );
+  }
+
+  showApply(openingId: string) {
+    return !this.showContinue(openingId) && !this.showCheckStatus(openingId);
+  }
 
   async ngOnInit() {
-    this.openings = await this.openingService.getAll();
+    [this.openings, this.currentUser] = await Promise.all([
+      this.openingService.getAll(),
+      this.userService.getCurrentUser(),
+    ]);
+
+    console.log(this.openings, this.currentUser);
   }
 }
