54cad3115aa308b8dd94c6343c0133c3c93973e2
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-22 20:20:29 -0500

    Add user-panel component to dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 import { UserInfo } from 'src/app/stores/user.reducer';
 
 @Component({
@@ -11,7 +12,7 @@ export class DashboardComponent {
   user: UserInfo | null = null;
   currentComp = 'openings';
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private userService: UserService) {
     const routerExtraState = this.router.getCurrentNavigation()?.extras?.state;
     if (routerExtraState && routerExtraState['user']) {
       this.user = routerExtraState['user'];
@@ -22,5 +23,9 @@ export class DashboardComponent {
     if (this.currentComp !== comp) {
       this.currentComp = comp;
     }
+  }
+
+  logout(): void {
+    this.userService.logout();
   }
 }
