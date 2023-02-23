6f6f1ca0bf38b036f276406f1fac07421793a523
Prasetyo Denny Wibowo <indocoffee@gmail.com>
2023-02-22 21:29:10 -0500

    Change applications array to completedApplications and incompleteApplications arrays

--- src/app/services/opening.service.ts
+++ src/app/services/opening.service.ts
@@ -15,7 +15,8 @@ export type Opening = {
   id: string;
   title: string;
   description: string;
-  applications: Application[];
+  completedApplications: Application[];
+  incompleteApplications: Application[];
 };
 
 export type Experience = {
@@ -37,14 +38,16 @@ export class OpeningService {
       title: 'Software Engineer',
       description:
         'XYZ is seeking a Developer for a remote contract-to-hire position with our thriving Fortune 100 life insurance client. This is primarily remote, but the candidate must be willing to come onsite in the NY or PA office once per quarter',
-      applications: [],
+      completedApplications: [],
+      incompleteApplications: [],
     },
     {
       id: '2',
       title: 'Support Manager',
       description:
         'This position is responsible for coordinating and facilitating the BCBSKS 12 week Big Room Planning(BRP)/collaborative planning efforts and support. This requires tight partnership with a variety of influencers and leaders across the organization including; Enterprise Planning team, PMO, Scrum Masters, Product Owners, ELT and CP3. This role helps ensure the company follows a regular cadence of formal, collaborative planning, with updates on prioritized work, other planning inputs, team planning, readouts, confidence votes, with regular reporting and risk monitoring with a variety of audiences, at all levels. The goals of BRP including transparency, systems learning and alignment by bringing multiple perspectives into the same room.',
-      applications: [],
+      completedApplications: [],
+      incompleteApplications: [],
     },
   ];
 
@@ -64,7 +67,7 @@ export class OpeningService {
 
     if (theOpening && user) {
       value.userId = user.email;
-      theOpening.applications.push(value);
+      theOpening.completedApplications.push(value);
     } else {
     }
   }
