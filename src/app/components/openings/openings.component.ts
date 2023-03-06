bb619fb3d490fdb1d772ca366b861bf27ed95466
Denny Wibowo <indocoffee@gmail.com>
2023-03-05 19:58:22 -0500

    Format changes

--- src/app/components/openings/openings.component.ts
+++ src/app/components/openings/openings.component.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { ApplicationService } from 'src/app/services/application.service';
-import {
-  Application,
-  ApplicationConnector,
-  Opening,
-  OpeningService,
-} from 'src/app/services/opening.service';
+import { Application, ApplicationConnector, Opening, OpeningService } from 'src/app/services/opening.service';
 import { UserService } from 'src/app/services/user.service';
 import { UserInfo } from 'src/app/stores/user.reducer';
 
@@ -20,24 +15,15 @@ export class OpeningsComponent {
   application: Application | null = null;
   currentUser: UserInfo | null = null;
 
-  constructor(
-    private openingService: OpeningService,
-    private applicationService: ApplicationService,
-    private userService: UserService
-  ) {}
+  constructor(private openingService: OpeningService, private applicationService: ApplicationService, private userService: UserService) {}
 
   async showContinue(openingId: string) {
     const existApplication = this.openings
       .find((opening) => opening.id === openingId)
-      ?.incompleteApplications.find(
-        (application: ApplicationConnector) =>
-          application.email === this.currentUser?.email
-      );
+      ?.incompleteApplications.find((application: ApplicationConnector) => application.email === this.currentUser?.email);
 
     if (existApplication !== undefined) {
-      this.application = await firstValueFrom(
-        this.applicationService.getById(existApplication.applicationId)
-      );
+      this.application = await firstValueFrom(this.applicationService.getById(existApplication.applicationId));
     }
 
     return existApplication !== undefined;
@@ -47,26 +33,17 @@ export class OpeningsComponent {
     return (
       this.openings
         .find((opening) => opening.id === openingId)
-        ?.completedApplications.find(
-          (application: ApplicationConnector) =>
-            application.email === this.currentUser?.email
-        ) !== undefined
+        ?.completedApplications.find((application: ApplicationConnector) => application.email === this.currentUser?.email) !== undefined
     );
   }
 
   showApply(openingId: string) {
-    return (
-      !this.showContinue(openingId.toString()) &&
-      !this.showCheckStatus(openingId.toString())
-    );
+    return !this.showContinue(openingId.toString()) && !this.showCheckStatus(openingId.toString());
   }
 
   async ngOnInit() {
-    [this.openings, this.currentUser] = await Promise.all([
-      this.openingService.getAll(),
-      this.userService.getCurrentUser(),
-    ]);
+    [this.openings, this.currentUser] = await Promise.all([this.openingService.getAll(), this.userService.getCurrentUser()]);
 
-    console.log(this.openings, this.currentUser);
+    //console.log('akua :', this.openings, this.currentUser);
   }
 }
